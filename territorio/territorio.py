2339c167dbaa89f8b16080229b56ae0ec3428985
Dayane Pabla <dayanepabla@gmail.com>
2014-05-20 21:08:22 -0300

    Finalização dos métodos da class Tile

--- territorio/territorio.py
+++ territorio/territorio.py
@@ -4,7 +4,23 @@ class Tile:
         self.x = x
         self.y = y
         self.land = land
+        # Referencia pro mapa do qual esse tile(self) faz parte.
         self.map = map
+
+    @property
+    def neighbors(self):
+        neighbors = []
+        for tile in self.maps.tiles
+            if self._is_neighbor_up(tile) or self._is_neighbor_down(tile) or \
+            self._is_neighbor_left(tile) or self._is_neighbor_right(tile):
+            neighbors.append(tile)
+
+        return neighbors
+
+    def is_coast(self):
+        for tile in neighbors
+            if tile.is_ocean()
+                return True
 
     def is_ocean(self):
         return not self.land
@@ -13,23 +29,19 @@ class Tile:
         return self.land
 
     def _is_neighbor_up(self, tile):
-        return self.x == tile.x && self.y == (tile.y + 1)
+        return self.x == tile.x and self.y == (tile.y + 1)
 
     def _is_neighbor_down(self, tile):
-      return self.x == tile.x && self.y == (tile.y - 1)
+      return self.x == tile.x and self.y == (tile.y - 1)
 
     def _is_neighbor_left(self, tile):
-      return self.y == tile.y && self.x == (tile.x - 1)
+      return self.y == tile.y and self.x == (tile.x - 1)
 
     def _is_neighbor_right(self, tile):
-      return self.y == tile.y && self.x == (tile.x + 1)
+      return self.y == tile.y and self.x == (tile.x + 1)
 
 class Map:
     def __init__(self, lines, columns):
         self.tiles = []
         self.lines = lines
         self.columns = columns
-
-
-
-
